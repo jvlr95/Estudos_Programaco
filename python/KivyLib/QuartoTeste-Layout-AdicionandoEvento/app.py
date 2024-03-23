e38bd7e09f9c9306672d279cd4d08fed64d6a1ad
jvlr9510 <jvitor.lr9510@gmail.com>
2024-03-22 23:48:23 -0300

    Criando interação com o botão salve

---
+++ python/KivyLib/QuartoTeste-Layout-AdicionandoEvento/app.py
@@ -0,0 +1,18 @@
+# Importa a classe App do módulo kivy.app
+from kivy.app import App
+# Importa a classe Button do módulo kivy.uix.button
+from kivy.uix.button import Button
+
+class MainApp(App):
+    def build(self):
+        button = Button(text='SALVE!!!',
+                        size_hint=(.5, .5),
+                        pos_hint={'center_x': .5, 'center_y': .5})
+        button.bind(on_press=self.onPressButton)
+        return button
+
+    def onPressButton(self, instance):
+        print('Você apertou o botão! êêêêêêêêêêê')
+
+if __name__ == '__main__':
+    MainApp().run()
